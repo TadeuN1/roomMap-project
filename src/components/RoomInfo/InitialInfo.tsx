c7feb36806a0540cb668723fc5cf608a94d30a67
TadeuN1 <tadeunrs@gmail.com>
2025-03-31 23:20:26 -0300

    return to project

--- src/components/RoomInfo/InitialInfo.tsx
+++ src/components/RoomInfo/InitialInfo.tsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import styles from './InitialInfo.module.css'
 
 const InitialInfo = () => {
+  const [teacher, setTeacher] = useState("")
+  const [room, setRoom] = useState("")
+
+  const test = () => {
+    console.log(teacher + room)
+  }
+
   return<div className={styles.container}>
       <h3> Insira os dados iniciais:</h3>
 
       <div className={styles.inputs}>
-        <input placeholder='PROFESSOR(A) RESPONSÁVEL' type='' />
-        <input placeholder='NOME DA TURMA/TURNO' type='' />
-        <input placeholder='Nº COLUNAS' type='' />
-        <input placeholder='Nº LINHAS' type='' />
-        <button> Começar </button>
+        <input placeholder='PROFESSOR(A) RESPONSÁVEL' type='text' onChange={(e) => setTeacher(e.target.value)} />
+        <input placeholder='NOME DA TURMA/TURNO' type='text' onChange={(e) => setRoom(e.target.value)} />
+        <button onClick={test}> Começar </button>
       </div>
       
 
